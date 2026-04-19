e8f441397eaef1a5a8743e58191729b542d0d48b
hot-life-plan <suzu12253810@gmail.com>
2026-04-19 19:23:16 +0900

    Update gallery mapping via manager

--- js/gallery_data.js
+++ js/gallery_data.js
@@ -1,22 +1,21 @@
-// ==========================================
-// 写真管理用リスト (画像を増やしたり減らしたりする場合はここを編集してください)
-// ※ここのリストは Cloudflare Worker (AI) によって本番環境で自動的に更新されます。
-// ==========================================
 const AVAILABLE_IMAGES = [
-        'img/guest/1776589216955_guest_photo.jpg',
-    'img/line_oa_chat_260404_192627.jpg',
-    'img/line_oa_chat_260404_192635.jpg',
-    'img/line_oa_chat_260404_192633.jpg',
-    'img/line_oa_chat_260404_192641.jpg',
-    'img/line_oa_chat_260404_192644.jpg',
-    'img/line_oa_chat_260404_192646.jpg',
-    'img/line_oa_chat_260404_192649.jpg',
-    'img/line_oa_chat_260404_192639.jpg',
-    'img/line_oa_chat_260404_192654.jpg',
-    'img/line_oa_chat_260404_192652.jpg',
-    'img/line_oa_chat_260404_192657.jpg',
-    'img/line_oa_chat_260404_192700.jpg',
-    'img/line_oa_chat_260404_192703.jpg',
-    'img/line_oa_chat_260404_192705.jpg',
-    'img/line_oa_chat_260404_192708.jpg'
+  "img/line_oa_chat_260404_192627.jpg",
+  "img/line_oa_chat_260404_192633.jpg",
+  "img/line_oa_chat_260404_192635.jpg",
+  "img/line_oa_chat_260404_192639.jpg",
+  "img/line_oa_chat_260404_192641.jpg",
+  "img/line_oa_chat_260404_192644.jpg",
+  "img/line_oa_chat_260404_192646.jpg",
+  "img/line_oa_chat_260404_192649.jpg",
+  "img/line_oa_chat_260404_192652.jpg",
+  "img/line_oa_chat_260404_192654.jpg",
+  "img/line_oa_chat_260404_192657.jpg",
+  "img/line_oa_chat_260404_192700.jpg",
+  "img/line_oa_chat_260404_192703.jpg",
+  "img/line_oa_chat_260404_192705.jpg",
+  "img/line_oa_chat_260404_192708.jpg",
+  "img/guest/1776581051613_guest_photo.jpg",
+  "img/guest/1776584076599_guest_photo.jpg",
+  "img/guest/1776585197649_guest_photo.jpg",
+  "img/guest/1776589216955_guest_photo.jpg"
 ];
